7233004a1dbb43ab4957e78bfada4a32f32e847c
aklesh-dev <arzunayavadu998@gmail.com>
2024-12-05 20:43:50 +0545

    feat: Navbar audio play btn and animate the navbar on scroll.
    - This feature will add a play button on the navbar which will play the audio.
    - toggle the audio play on/off
    - active the indicator line on play of audio.
    - To get the current scroll position,  react-use package is used.
    - And gsap to use to animate the navbar on scroll, which shows the navbar on y-axis scroll.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,58 @@
 import React from 'react'
 import Button from './Button';
 import { TiLocationArrow } from 'react-icons/ti';
-
+import { useWindowScroll } from 'react-use';
+import gsap from 'gsap';
 
 const navItems = ['Nexus', 'Vault', 'Prologue', 'About', 'Contact'];
 
 const Navbar = () => {
   const [isAudioPlaying, setIsAudioPlaying] = React.useState(false);
   const [isIndicatorActive, setIsIndicatorActive] = React.useState(false);
+  const [lastScrollY, setLastScrollY] = React.useState(0);
+  const [isNavVisible, setIsNavVisible] = React.useState(true);
 
   const navContainerRef = React.useRef(null);
   const audioElementRef = React.useRef(null);
+  // --scroll event--
+  const { y: currentScrollY } = useWindowScroll();
 
-  const toggleAudioIndicator = () => { };
+  React.useEffect(() => {
+    if(currentScrollY === 0) {
+      setIsNavVisible(true);  // --show navbar on scroll up
+      navContainerRef.current.classList.remove('floating-nav'); // --remove floating effect on scroll up
+    } else if (currentScrollY > lastScrollY) {
+      setIsNavVisible(false); // --hide navbar on scroll down
+      navContainerRef.current.classList.add('floating-nav'); // --add floating effect on scroll down
+    } else if (currentScrollY < lastScrollY){
+      setIsNavVisible(true); // --show navbar on scroll up
+      navContainerRef.current.classList.add('floating-nav'); // --add floating effect on scroll up
+    }
+    setLastScrollY(currentScrollY); // --update last scroll position
+  }, [currentScrollY]);
+
+  // --animate navbar || hide/show on scroll --
+  React.useEffect(() => {
+    gsap.to(navContainerRef.current, {
+      y: isNavVisible ? 0 : -100,
+      opacity: isNavVisible ? 1 : 0,
+      duration: 0.2,
+      ease: 'power3.inOut',
+    });
+  },[isNavVisible]);
+
+  const toggleAudioIndicator = () => {
+    setIsAudioPlaying((prev) => !prev);
+    setIsIndicatorActive((prev) => !prev);
+  };
+
+  React.useEffect(() => {
+    if (isAudioPlaying) {
+      audioElementRef.current.play();
+    } else {
+      audioElementRef.current.pause();
+    }
+  }, [isAudioPlaying]);
 
   return (
     <section ref={navContainerRef} className="fixed inset-x-0 top-4 z-50 h-16 border-none transition-all duration-300 md:inset-x-6">
@@ -40,17 +80,17 @@ const Navbar = () => {
             </div>
             {/* --audio play btn-- */}
             <button className="ml-10 flex items-center space-x-0.5" onClick={toggleAudioIndicator}>
-              <audio src="/audio/loop.mp3" loop className='hidden' ref={audioElementRef}>
-                {
-                  [1, 2, 3, 4].map((bar) => (
-                    <div
-                      key={bar}
-                      className={`indicator-line ${isIndicatorActive ? "active" : ""} `}
-                      style={{ animationDelay: `${bar * 0.1}s` }}
-                    />
-                  ))
-                }
-              </audio>
+              <audio src="/audio/loop.mp3" loop className='hidden' ref={audioElementRef} />
+              {
+                [1, 2, 3, 4].map((bar) => (
+                  <div
+                    key={bar}
+                    className={`indicator-line ${isIndicatorActive ? "active" : ""} `}
+                    style={{ animationDelay: `${bar * 0.1}s` }}
+                  />
+                ))
+              }
+
             </button>
           </div>
         </nav>
